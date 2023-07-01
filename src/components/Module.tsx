1c7f2c921049cb765900f54d8e8ec474cc3bf981
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-07-01 15:18:28 -0300

    Destacando aula atual

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -4,15 +4,19 @@ import { Lesson } from "./Lesson";
 import { useAppSelector } from "../store";
 import { useDispatch } from "react-redux";
 import { play } from "../store/slices/player";
-
 interface ModuleProps {
   moduleIndex: number;
   title: string;
   amountOfLessons: number;
 }
-
 export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   const dispatch = useDispatch()
+
+  const { currentModuleIndex, currentLessonIndex } = useAppSelector(state => {
+    const { currentModuleIndex, currentLessonIndex } = state.player
+
+    return { currentModuleIndex, currentLessonIndex }
+  })
 
   const lessons = useAppSelector(state => {
     return state.player.course.modules[moduleIndex].lessons
@@ -29,15 +33,18 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
         </div>
         <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
       </Collapsible.Trigger>
-
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
           {lessons.map((lesson, lessonIndex) => {
+            const isCurrent = currentModuleIndex === moduleIndex &&
+              currentLessonIndex === lessonIndex
+
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
+                isCurrent={isCurrent}
                 onPlay={() => dispatch(play([moduleIndex, lessonIndex]))}
               />
             )
